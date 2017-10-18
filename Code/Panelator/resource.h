d601118a350c404da9c376580b957b44fb40c432
FlorentE96 <flo.ecochard@gmail.com>
2017-10-17 23:10:57 -0200

    First commit

---
+++ Code/Panelator/resource.h
@@ -0,0 +1,8 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDD_DIALOG1                             100
+#define IDR_MENU1                               101
+#define CANCEL_CANCEL                           40000
+#define OK_OK                                   40002
---
+++ Resources/Pan_res/resource.h
@@ -0,0 +1,8 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDD_DIALOG1                             100
+#define IDR_MENU1                               101
+#define IDOK                                    40000
+#define IDCANCEL                                40001
