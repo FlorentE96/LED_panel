048823ef17043b1f4ff232031d687eb1e8f310d2
FlorentE96 <flo.ecochard@gmail.com>
2017-10-23 21:39:40 -0200

    buttons working

--- Code/Panelator/main.cpp
+++ Code/Panelator/main.cpp
@@ -1,10 +1,11 @@
 #include <windows.h>
+#include <stdio.h>
 #include "resource.h"
 const char g_szClassName[] = "myWindowClass";
 
 // Step 4: the Window Procedure
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
+{   /*this part of the code process the "events" since it's a event oriented language*/
     switch(msg)
     {
         case WM_CLOSE:
@@ -20,10 +21,10 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-    LPSTR lpCmdLine, int nCmdShow)
+    LPSTR lpCmdLine, int nCmdShow) /*these parameters are given by the OS to the application so it can run on top of it*/
 {
     WNDCLASSEX wc;
-    HWND hwnd;
+    HWND hwnd;      /*window handler*/
     MSG Msg;
 
     //Step 1: Registering the Window Class
@@ -51,9 +52,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     hwnd = CreateWindowEx(
         WS_EX_CLIENTEDGE,
         g_szClassName,
-        "The title of my window",
+        "The title of my window",  /*window name*/
         WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 240, 120,
+        CW_USEDEFAULT, CW_USEDEFAULT, 500, 500, /*the size of the window*/
         NULL, NULL, hInstance, NULL);
 
     if(hwnd == NULL)
@@ -63,12 +64,38 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
         return 0;
     }
 
-    ShowWindow(hwnd, nCmdShow);
-    UpdateWindow(hwnd);
+    //====== buttons and shit
+    HWND hwndButton = CreateWindow("BUTTON",   /*a button and every other widget is called "window" but have different parameters*/
+                                   "OK",
+                                   WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+                                   10, /*x*/
+                                   10, /*y*/
+                                   30, /*size x*/
+                                   30, /*size y*/
+                                   hwnd,
+                                   (HMENU)TEST_BUTTON,
+                                   (HINSTANCE)GetWindowLong(hwnd, GWL_HINSTANCE),
+                                   NULL);
+
+    HWND hwndButton2 = CreateWindow("BUTTON",
+                                   "BUT2",
+                                   WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+                                   10,
+                                   50,
+                                   30,
+                                   30,
+                                   hwnd,
+                                   (HMENU)TEST_BUTTON,
+                                   (HINSTANCE)GetWindowLong(hwnd, GWL_HINSTANCE),
+                                   NULL);
+
+
+    ShowWindow(hwnd, nCmdShow);  /*needs to be done to create a windows*/
+    UpdateWindow(hwnd);          /*should be done everytime a "widget" is added*/
 
     // Step 3: The Message Loop
     while(GetMessage(&Msg, NULL, 0, 0) > 0)
-    {
+    {   /*this part of the code is like a while loop that runs everytime*/
         TranslateMessage(&Msg);
         DispatchMessage(&Msg);
     }
--- Code/Panelator/resource.h
+++ Code/Panelator/resource.h
@@ -4,5 +4,6 @@
 
 #define IDD_DIALOG1                             100
 #define IDR_MENU1                               101
+#define TEST_BUTTON                             200
 #define CANCEL_CANCEL                           40000
 #define OK_OK                                   40002
