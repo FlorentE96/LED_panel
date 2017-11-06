a882b8176795754ff0a5ead9ea7ea5ddd11c5e58
FlorentE96 <flo.ecochard@gmail.com>
2017-11-06 18:20:23 -0200

    [General]added colors file header

---
+++ Code/Panel_manager/colors.h
@@ -0,0 +1,13 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+COLORREF    clrBlue = RGB(25, 55, 200);
+COLORREF    clrRed = RGB(255, 0, 0);
+COLORREF    clrWhite = RGB(255, 255, 255);
+COLORREF    clrYellow = RGB(255, 255, 0);
+COLORREF    clrOrange = RGB(255, 165, 0);
+COLORREF    clrGreen = RGB(0, 255, 0);
+COLORREF    clrBlack = RGB(0, 0, 0);
+COLORREF    clrAqua = RGB(128, 255, 255);
+
+#endif
